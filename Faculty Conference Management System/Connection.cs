fc87eeb5df6a52e6d037fdeb1033e4317c4be76e
YosefAhmed <youssefahmed.cis@gmail.com>
2020-04-11 15:05:26 +0200

    Connection class is added

---
+++ Faculty Conference Management System/Connection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
+namespace Faculty_Conference_Management_System
+{
+	class Connection
+	{
+		public string conStr { get; set; }
+		Connection()
+		{
+			conStr = "Data Source=orcl; User Id=hr; Password=hr;";
+		}
+	}
+}
