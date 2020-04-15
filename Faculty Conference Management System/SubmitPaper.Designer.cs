6e6c06146aa2eb4688559bc7bf3912f0bec95e66
MohamedKamal98 <32045725+MohamedKamal98@users.noreply.github.com>
2020-04-15 17:05:30 +0200

    submit paper

---
+++ Faculty Conference Management System/SubmitPaper.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Faculty_Conference_Management_System
+{
+    partial class SubmitPaper
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Title_textBox = new System.Windows.Forms.TextBox();
+            this.category_textBox = new System.Windows.Forms.TextBox();
+            this.content_textBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(347, 360);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "submit ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(104, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Title ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(104, 170);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "content";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(107, 264);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "category ";
+            // 
+            // Title_textBox
+            // 
+            this.Title_textBox.Location = new System.Drawing.Point(204, 88);
+            this.Title_textBox.Name = "Title_textBox";
+            this.Title_textBox.Size = new System.Drawing.Size(100, 20);
+            this.Title_textBox.TabIndex = 4;
+            // 
+            // category_textBox
+            // 
+            this.category_textBox.Location = new System.Drawing.Point(204, 261);
+            this.category_textBox.Name = "category_textBox";
+            this.category_textBox.Size = new System.Drawing.Size(100, 20);
+            this.category_textBox.TabIndex = 5;
+            // 
+            // content_textBox
+            // 
+            this.content_textBox.Location = new System.Drawing.Point(204, 170);
+            this.content_textBox.Name = "content_textBox";
+            this.content_textBox.Size = new System.Drawing.Size(100, 20);
+            this.content_textBox.TabIndex = 6;
+            // 
+            // SubmitPaper
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.content_textBox);
+            this.Controls.Add(this.category_textBox);
+            this.Controls.Add(this.Title_textBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "SubmitPaper";
+            this.Text = "SubmitPaper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Title_textBox;
+        private System.Windows.Forms.TextBox category_textBox;
+        private System.Windows.Forms.TextBox content_textBox;
+    }
+}
---
+++ Faculty Conference Management System/SubmitPaper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+
+namespace Faculty_Conference_Management_System
+{
+    public partial class SubmitPaper : Form
+    {
+        public SubmitPaper()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Connection c = new Connection();
+            OracleCommand cmd = new OracleCommand();
+            OracleConnection con = new OracleConnection(c.conStr);
+            cmd.Connection = con;
+            cmd.CommandText = "insert into PAPER values (:PAPER_TITLE,:PAPER_CONTENT,:)";
+            cmd.Parameters.Add("PAPER_TITLE", Title_textBox.Text);
+            cmd.Parameters.Add("PAPER_CONTENT", content_textBox.Text);
+            int r = cmd.ExecuteNonQuery();
+            if (r!=-1)
+            {
+                MessageBox.Show("Submittion is succussful");
+            }
+        }
+    }
+}
